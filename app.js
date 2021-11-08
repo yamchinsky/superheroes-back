abc4e5259024d01dcea7db7dac24a87f0c1373cf
yamchinsky <66372922+yamchinsky@users.noreply.github.com>
2021-11-08 20:35:20 +0200

    swagger

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const logger = require('morgan');
 const cors = require('cors');
 const api = require('./routes/api');
 
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./config/swagger.json');
+
 require('dotenv').config();
 const app = express();
 
@@ -25,6 +28,8 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/api/v1/auth', api.auth);
 
